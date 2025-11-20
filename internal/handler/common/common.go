1915f8448a272be358fd4838b850346f81b1d1f1
whalefell <63758849+WhaleFell@users.noreply.github.com>
2025-11-20 22:28:45 +0800

    feat: add map server index.

--- internal/handler/common/common.go
+++ internal/handler/common/common.go
@@ -1,12 +1,24 @@
 // common handler
+// 公共处理器
 package common
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
 
+	_ "embed"
+
+	"github.com/labstack/echo/v4"
+)
+
+//go:embed index.html
+var indexHTML string
+
+// Index serves the embedded homepage to echo context
+// Index 函数向 Echo 上下文返回嵌入的首页内容
 func Index(c echo.Context) error {
-	return c.String(200, "go-map-proxy server is running!")
+	return c.HTML(http.StatusOK, indexHTML)
 }
 
 func HealthCheck(c echo.Context) error {
-	return c.String(200, "go-map-proxy server is healthy!")
+	return c.String(http.StatusOK, "go-map-proxy server is healthy!")
 }
